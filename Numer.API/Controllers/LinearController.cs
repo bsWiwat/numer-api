3b900fedd349b860947e6bd1ac5f7b5d2d5a118d
bsWiwat <bs.wiwat@gmail.com>
2024-10-25 14:54:20 +0700

    Update(controller): new gauss controller

--- Numer.API/Controllers/LinearController.cs
+++ Numer.API/Controllers/LinearController.cs
@@ -7,6 +7,8 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Numer.Core.Features.Linear.Commands.Cramer;
+using Numer.Core.Features.Linear.Commands.GaussElimination;
+using Numer.Core.Features.Linear.Commands.GaussJordan;
 using Numer.Core.Features.Linear.Commands.JacobiMethod;
 using Numer.Domain.Entities;
 
@@ -24,6 +26,38 @@ namespace Numer.API.Controllers {
         public async Task<ActionResult<MatrixResult>> SolveCramer([FromBody] CramerCommand request) {
             try {
                 var command = new CramerCommand {
+                    MatrixA = request.MatrixA,
+                    ArrayB = request.ArrayB,
+                };
+
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("gauss")]
+        public async Task<ActionResult<GaussResult>> SolveGauss([FromBody] GaussEliminationCommand request) {
+            try {
+                var command = new GaussEliminationCommand {
+                    MatrixA = request.MatrixA,
+                    ArrayB = request.ArrayB,
+                };
+
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("gauss-jordan")]
+        public async Task<ActionResult<GaussResult>> SolveGaussJordan([FromBody] GaussJordanCommand request) {
+            try {
+                var command = new GaussJordanCommand {
                     MatrixA = request.MatrixA,
                     ArrayB = request.ArrayB,
                 };
