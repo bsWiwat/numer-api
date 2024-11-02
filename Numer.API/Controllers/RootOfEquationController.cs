2d23338bbdce40e91c5aad27b930be50e5f35ef8
bsWiwat <bs.wiwat@gmail.com>
2024-11-03 02:53:45 +0700

    Update(controller): root controller.

--- Numer.API/Controllers/RootOfEquationController.cs
+++ Numer.API/Controllers/RootOfEquationController.cs
@@ -7,6 +7,9 @@ using Numer.API.Helper;
 using Numer.Core.Features.RootOfEquation.Commands.BisectionMethod;
 using Numer.Core.Features.RootOfEquation.Commands.FalsePositionMethod;
 using Numer.Core.Features.RootOfEquation.Commands.GraphicalMethod;
+using Numer.Core.Features.RootOfEquation.Commands.NewtonRaphsonMethod;
+using Numer.Core.Features.RootOfEquation.Commands.OnePointMethod;
+using Numer.Core.Features.RootOfEquation.Commands.SecantMethod;
 using Numer.Core.Models;
 using Numer.Domain.Entities;
 
@@ -33,8 +36,8 @@ namespace Numer.API.Controllers {
 
                 var command = new BisectionCommand {
                     Function = parsedFunction,
-                    LowerBound = request.X0,
-                    UpperBound = request.X1,
+                    LowerBound = request.LowerBound,
+                    UpperBound = request.UpperBound,
                     Tolerance = request.Tolerance,
                     MaxIterations = request.MaxIterations
                 };
@@ -60,8 +63,8 @@ namespace Numer.API.Controllers {
 
                 var command = new FalsePositionCommand {
                     Function = parsedFunction,
-                    LowerBound = request.X0,
-                    UpperBound = request.X1,
+                    LowerBound = request.LowerBound,            
+                    UpperBound = request.UpperBound,
                     Tolerance = request.Tolerance,
                     MaxIterations = request.MaxIterations
                 };
@@ -87,8 +90,87 @@ namespace Numer.API.Controllers {
 
                 var command = new GraphicalCommand {
                     Function = parsedFunction,
-                    LowerBound = request.X0,
-                    UpperBound = request.X1,
+                    LowerBound = request.LowerBound,
+                    UpperBound = request.UpperBound,
+                    Tolerance = request.Tolerance,
+                    MaxIterations = request.MaxIterations
+                };
+
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("secant")]
+        public async Task<ActionResult<RootResult>> SolveSecant([FromBody] RootRequest request) {
+            try {
+                string parsedExpression = ExpressionUtils.ConvertExponentiation(request.FunctionExpression);
+
+                Func<double, double> parsedFunction = x => {
+                    var expression = new Expression(parsedExpression);
+                    expression.Parameters["x"] = x;
+                    return Convert.ToDouble(expression.Evaluate());
+                };
+
+                var command = new SecantCommand {
+                    Function = parsedFunction,
+                    LowerBound = request.LowerBound,
+                    UpperBound = request.UpperBound,
+                    Tolerance = request.Tolerance,
+                    MaxIterations = request.MaxIterations
+                };
+
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("one-point")]
+        public async Task<ActionResult<RootResult>> SolveOnePoint([FromBody] RootRequest request) {
+            try {
+                string parsedExpression = ExpressionUtils.ConvertExponentiation(request.FunctionExpression);
+
+                Func<double, double> parsedFunction = x => {
+                    var expression = new Expression(parsedExpression);
+                    expression.Parameters["x"] = x;
+                    return Convert.ToDouble(expression.Evaluate());
+                };
+
+                var command = new OnePointCommand {
+                    Function = parsedFunction,
+                    XInitial = request.UpperBound,
+                    Tolerance = request.Tolerance,
+                    MaxIterations = request.MaxIterations
+                };
+
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("newton")]
+        public async Task<ActionResult<RootResult>> SolveNewton([FromBody] RootRequest request) {
+            try {
+                string parsedExpression = ExpressionUtils.ConvertExponentiation(request.FunctionExpression);
+
+                Func<double, double> parsedFunction = x => {
+                    var expression = new Expression(parsedExpression);
+                    expression.Parameters["x"] = x;
+                    return Convert.ToDouble(expression.Evaluate());
+                };
+
+                var command = new NewtonRaphsonCommand {
+                    Function = parsedFunction,
+                    XInitial = request.UpperBound,
                     Tolerance = request.Tolerance,
                     MaxIterations = request.MaxIterations
                 };
